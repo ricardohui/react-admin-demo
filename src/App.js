fbe0af182a55fed68b244c8f35f02348bcb22bd2
Ricardo Hui <rcdhui@gmail.com>
2021-06-06 16:42:18 +0800

    support edit and create post

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from "./users";
-import { PostList } from "./posts";
+import { PostCreate, PostEdit, PostList } from "./posts";
 
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name={"posts"} list={PostList}/>
+  <Resource name={"posts"} list={PostList} edit={PostEdit} create={PostCreate}/>
   <Resource name={"users"} list={UserList}/>
 </Admin>
 
--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,17 @@
 import * as React from "react";
-import {List, Datagrid, TextField, ReferenceField, EditButton} from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
 
 export const PostList = props =>(
     <List {...props}>
@@ -14,3 +26,24 @@ export const PostList = props =>(
     </List>
 )
 
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id"/>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="name"/></ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id"/>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="name"/></ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Create>
+);
