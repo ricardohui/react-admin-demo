caba04d08216d5aa64db0599ce5d331246aaccc8
Ricardo Hui <rcdhui@gmail.com>
2021-06-06 16:06:33 +0800

    custom-made field

--- src/users.js
+++ src/users.js
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, UrlField } from 'react-admin';
+import CustomUrlField from './CustomUrlField';
 
 
-export const UserList = props => (<List {...props}>
-    <Datagrid rowClick="edit">
-        <TextField source="id"/>
-        <TextField source={"name"}/>
-        <TextField source={"username"}/>
-        <EmailField source={"email"}/>
-        <TextField source={"address.street"}/>
-        <TextField source={"phone"}/>
-        <TextField source="website"></TextField>
-        <TextField source="company.name"></TextField>
-    </Datagrid>
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id"/>
+            <TextField source={"name"}/>
+            <TextField source={"username"}/>
+            <EmailField source={"email"}/>
+            <TextField source={"address.street"}/>
+            <TextField source={"phone"}/>
+            <CustomUrlField source="website"></CustomUrlField>
+            <TextField source="company.name"></TextField>
+        </Datagrid>
 </List>);
